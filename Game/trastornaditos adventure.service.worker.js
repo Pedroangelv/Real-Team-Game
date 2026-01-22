a61ed64b9dc163474b8cdc52fec79567b739c3fc
Pedroangelv <grijalbaangel393@gmail.com>
2026-01-21 22:51:17 -0500

    Build 21/01/26

--- Game/trastornaditos adventure.service.worker.js
+++ Game/trastornaditos adventure.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1767198912|128992616';
+const CACHE_VERSION = '1769053679|1645243694';
 /** @type {string} */
 const CACHE_PREFIX = 'RT_GAME-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
